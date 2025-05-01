b3eefd7ae77b38187cf0b42bafa1881bbfc0b588
tharakajayasundara2005 <161567335+Sahantj2005@users.noreply.github.com>
2025-05-01 19:53:39 +0530

    Update index_header.php

--- index_header.php
+++ index_header.php
@@ -192,10 +192,22 @@
                                 <i class="ph ph-magnifying-glass"></i>
                             </span>
                         </button>
+                        <?php
+                    if (isset($_SESSION["user_vec"]["email"])) {
+                        try {
+                            $cc = Database::Search("SELECT * FROM `cart` WHERE `user_email`='" . $_SESSION["user_vec"]["email"] . "'");
+                            $ccn = $cc->num_rows; // corrected: should be `num_rows` not `num_row`
+                        } catch (Exception $e) {
+                            $ccn = "..";
+                        }
+                    } else {
+                        $ccn = "..";
+                    }
+                    ?>
                         <a href="cart.php" class="flex-align flex-column gap-8 item-hover-two">
                             <span class="text-2xl text-white d-flex position-relative me-6 mt-6 item-hover__text">
                                 <i class="ph ph-shopping-cart-simple"></i>
-                                <span class="w-16 h-16 flex-center rounded-circle bg-main-two-600 text-white text-xs position-absolute top-n6 end-n4">2</span>
+                                <span class="w-16 h-16 flex-center rounded-circle bg-main-two-600 text-white text-xs position-absolute top-n6 end-n4"><?php echo   $ccn ; ?></span>
                             </span>
                             <span class="text-md text-white item-hover__text d-none d-lg-flex">Cart</span>
                         </a>
